cc207c7cb460b23a9fd3e9a570f83884dbebacd5
Tomohiro Matsuyama <tomo@cx4a.org>
2010-03-14 01:38:18 +0900

    Fix order of ac-source-features and memo bugs.

--- auto-complete-config.el
+++ auto-complete-config.el
@@ -235,7 +235,7 @@
   (add-to-list 'ac-sources 'ac-source-filename))
 
 (defun ac-emacs-lisp-mode-setup ()
-  (setq ac-sources (append '(ac-source-functions ac-source-yasnippet ac-source-variables ac-source-symbols ac-source-features) ac-sources)))
+  (setq ac-sources (append '(ac-source-features ac-source-functions ac-source-yasnippet ac-source-variables ac-source-symbols) ac-sources)))
 
 (defun ac-cc-mode-setup ()
   (setq ac-sources (append '(ac-source-yasnippet ac-source-gtags) ac-sources)))
