6f9552aca0b50a7919eb2217fe5aa66c5ff2edbe
Tomohiro Matsuyama <tomo@cx4a.org>
2010-03-20 18:23:52 +0900

    Fix tooltip not to hide automatically.

--- auto-complete.el
+++ auto-complete.el
@@ -1080,7 +1080,7 @@ that have been made before in this function."
              (featurep 'pos-tip))
         (let ((doc (popup-menu-documentation ac-menu)))
           (when (and doc (fboundp 'pos-tip-show))
-            (pos-tip-show doc nil (popup-child-point ac-menu 0) nil nil nil nil nil 0)))
+            (pos-tip-show doc nil (popup-child-point ac-menu 0) nil -1 nil nil nil 0)))
       (setq ac-quick-help
             (popup-menu-show-quick-help ac-menu nil
                                         :point ac-point
@@ -1104,7 +1104,7 @@ that have been made before in this function."
                  (eq (window-system) 'x)
                  (featurep 'pos-tip)
                  (fboundp 'pos-tip-show))
-            (pos-tip-show doc nil point nil nil nil nil nil 0)
+            (pos-tip-show doc nil point nil -1)
           (popup-tip doc
                      :point point
                      :around t
