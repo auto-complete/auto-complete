272c176d392a49f91b8687aefd40c809a32ed261
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2010-03-21 15:59:30 +0900

    Rewrote/added functions for ac-source-symbols

--- auto-complete.el
+++ auto-complete.el
@@ -1560,16 +1560,84 @@ that have been made before in this function."
 (defvar ac-symbols-cache nil)
 (ac-clear-variable-every-10-minutes 'ac-symbols-cache)
 
+(defun ac-symbol-file (symbol type)
+  (if (fboundp 'find-lisp-object-file-name)
+      (find-lisp-object-file-name symbol type)
+    (let ((file-name (describe-simplify-lib-file-name
+                      (symbol-file symbol type))))
+      (when (equal file-name "loaddefs.el")
+        ;; Find the real def site of the preloaded object.
+        (let ((location (condition-case nil
+                            (if (eq type 'defun)
+                                (find-function-search-for-symbol symbol nil
+                                                                 "loaddefs.el")
+                              (find-variable-noselect symbol file-name))
+                          (error nil))))
+          (when location
+            (with-current-buffer (car location)
+              (when (cdr location)
+                (goto-char (cdr location)))
+              (when (re-search-backward
+                     "^;;; Generated autoloads from \\(.*\\)" nil t)
+                (setq file-name (match-string 1)))))))
+      (if (and (null file-name)
+               (or (eq type 'defun)
+                   (integerp (get symbol 'variable-documentation))))
+          ;; It's a object not defined in Elisp but in C.
+          (if (get-buffer " *DOC*")
+              (if (eq type 'defun)
+                  (help-C-file-name (symbol-function symbol) 'subr)
+                (help-C-file-name symbol 'var))
+            'C-source)
+        file-name))))
+
 (defun ac-symbol-documentation (symbol)
   (if (stringp symbol)
       (setq symbol (intern-soft symbol)))
-  (or (ignore-errors (documentation symbol t))
-      (ignore-errors (documentation-property symbol 'variable-documentation t))))
+  (ignore-errors
+    (with-temp-buffer
+      (let ((standard-output (current-buffer)))
+        (prin1 symbol)
+        (princ " is ")
+        (cond
+         ((fboundp symbol)
+          (describe-function-1 symbol)
+          (buffer-string))
+         ((boundp symbol)
+          (let ((file-name  (ac-symbol-file symbol 'defvar)))
+            (princ "a variable defined in `")
+            (princ (if (eq file-name 'C-source)
+                       "C source code"
+                     (file-name-nondirectory file-name)))
+            (princ "'.\n\n")
+            (princ (or (documentation-property symbol 'variable-documentation t)
+                       "Not documented."))
+            (buffer-string)))
+         ((facep symbol)
+          (let ((file-name  (ac-symbol-file symbol 'defface)))
+            (princ "a face defined in `")
+            (princ (if (eq file-name 'C-source)
+                       "C source code"
+                     (file-name-nondirectory file-name)))
+            (princ "'.\n\n")
+            (princ (or (documentation-property symbol 'face-documentation t)
+                       "Not documented."))
+            (buffer-string)))
+         (t
+          (let ((doc (documentation-property symbol 'group-documentation t)))
+            (when doc
+              (princ "a group.\n\n")
+              (princ doc)
+              (buffer-string)))))))))
 
 (defun ac-symbol-candidates ()
   (or ac-symbols-cache
       (setq ac-symbols-cache
-            (loop for x being the symbols collect (symbol-name x)))))
+            (loop for x being the symbols
+                  if (or (fboundp x)
+                         (boundp x)
+                         (symbol-plist x))
+                  collect (symbol-name x)))))
 
 (ac-define-source symbols
   '((candidates . ac-symbol-candidates)
