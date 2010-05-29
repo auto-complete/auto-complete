0df3e919a44b4073a58a52e350826141403c2c1c
Tomohiro Matsuyama <tomo@cx4a.org>
2010-05-29 18:53:47 +0900

    Added scheme-mode dictionary.

--- auto-complete.el
+++ auto-complete.el
@@ -162,7 +162,10 @@
 (defcustom ac-modes
   '(emacs-lisp-mode
     lisp-interaction-mode
-    c-mode cc-mode c++-mode clojure-mode java-mode
+    c-mode cc-mode c++-mode
+    java-mode clojure-mode scala-mode
+    scheme-mode
+    ocaml-mode tuareg-mode
     perl-mode cperl-mode python-mode ruby-mode
     ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode
     makefile-mode sh-mode fortran-mode f90-mode ada-mode
