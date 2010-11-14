bc1e6371a128e40d4326b27d473ece8f8688417a
Tomohiro Matsuyama <tomo@cx4a.org>
2010-11-14 17:29:16 +0900

    Prepare for release 1.3.1.

--- auto-complete-config.el
+++ auto-complete-config.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
 ;; Keywords: convenience
-;; Version: 1.3
+;; Version: 1.3.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
