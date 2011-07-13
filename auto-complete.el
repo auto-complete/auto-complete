a45aee86d7e2bfd6f5940987591f5791ca0437f8
Steve Purcell <steve@sanityinc.com>
2011-07-13 14:04:50 +0100

    Fix ac-symbol-documentation for functions in Emacs HEAD
    Looks like 'describe-function-1 now raises an error if current-buffer
    is not in help-mode, so 'ac-symbol-documentation fails to gather the info
    needed for the help popup.
    
    The fix in this commit works well for me.

--- auto-complete.el
+++ auto-complete.el
@@ -1784,8 +1784,7 @@ completion menu. This workaround stops that annoying behavior."
          ((fboundp symbol)
           ;; import help-xref-following
           (require 'help-mode)
-          (let ((help-xref-following t))
-            (describe-function-1 symbol))
+          (describe-function-1 symbol)
           (buffer-string))
          ((boundp symbol)
           (let ((file-name  (ac-symbol-file symbol 'defvar)))
