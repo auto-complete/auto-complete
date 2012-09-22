35c8174e426d7cc590b84dfee067ff0468c7378c
Christopher Monsanto <chris@monsan.to>
2012-09-22 04:34:36 -0400

    Get ac-yasnippet to byte compile.

--- sources/ac-yasnippet.el
+++ sources/ac-yasnippet.el
@@ -15,6 +15,8 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(require 'yasnippet)
+
 (defface ac-yasnippet-candidate-face
   '((t (:background "sandybrown" :foreground "black")))
   "Face for yasnippet candidate."
