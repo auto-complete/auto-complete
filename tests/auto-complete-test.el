78136fabcc8ffa0bf7a3a7a39f00dc6551a599e6
Syohei YOSHIDA <syohex@gmail.com>
2014-01-24 11:02:20 +0900

    Add test case same as #166
    We cannot merge #166 patches. I fixed it for merging to current source.

--- tests/auto-complete-test.el
+++ tests/auto-complete-test.el
@@ -14,8 +14,10 @@
        (switch-to-buffer (current-buffer))
        (auto-complete-mode 1)
        (emacs-lisp-mode)
-       ,@body
-       (ac-menu-delete)
+       (unwind-protect
+           (progn ,@body)
+        (when ac-menu
+          (ac-menu-delete)))
        )))
 
 (ert-deftest ac-test-simple-invocation ()
@@ -32,6 +34,31 @@
       (auto-complete)
       (should (popup-live-p ac-menu))
       (should (equal (popup-list ac-menu) '("Foo" "FooBar")))
+      )))
+
+(ert-deftest ac-test-single-candidate ()
+  (ac-test-with-common-setup
+    (let ((ac-source-test
+           '((candidates list "Foo" "FooBar" "Bar" "Baz" "LongLongLine"))))
+      (setq ac-sources '(ac-source-test))
+      (should-not (popup-live-p ac-menu))
+      (should (eq ac-menu nil))
+      (insert "FooB")
+      (auto-complete)
+      (should-not (popup-live-p ac-menu))
+      (should (eq ac-menu nil))
+      (should (string= (buffer-string) "FooBar"))
+      )))
+
+(ert-deftest ac-test-complete-common-part ()
+  (ac-test-with-common-setup
+    (let ((ac-source-test
+           '((candidates list "Foo" "FooBar" "Bar" "Baz" "LongLongLine"))))
+      (setq ac-sources '(ac-source-test))
+      (execute-kbd-macro "Fo")
+      (auto-complete)
+      (ac-stop)
+      (should (string= (buffer-string) "Foo"))
       )))
 
 (ert-deftest ac-test-simple-update ()
@@ -64,3 +91,33 @@
      (ac-stop)
      (should (string= (buffer-string) "Foo"))
      )))
+
+(ert-deftest ac-test-update-no-candidate ()
+  (ac-test-with-common-setup
+    (let ((ac-source-test
+           '((candidates list "Foo" "FooBar" "Bar" "Baz" "LongLongLine"))))
+      (setq ac-sources '(ac-source-test))
+      (should-not (popup-live-p ac-menu))
+      (should (eq ac-menu nil))
+      (insert "Foo")
+      (auto-complete)
+      (execute-kbd-macro "A")
+      (ac-update-greedy)
+      (should-not (popup-live-p ac-menu))
+      (should (eq ac-menu nil))
+      (should (string= (buffer-string) "FooA"))
+      )))
+
+(ert-deftest ac-test-default-selection ()
+  (ac-test-with-common-setup
+    (let ((ac-source-test
+           '((candidates list "Action1" "Action2" "Action3"))))
+      (setq ac-sources '(ac-source-test))
+      (should-not (popup-live-p ac-menu))
+      (should (eq ac-menu nil))
+      (insert "Action")
+      (auto-complete)
+      (should (popup-live-p ac-menu))
+      (should (equal (popup-list ac-menu) '("Action1" "Action2" "Action3")))
+      (should (equal (ac-selected-candidate) "Action1"))
+      )))
