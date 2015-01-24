841ca50316f21aef7c05bc40a88f28f80ec67f26
Tomohiro Matsuyama <tomo@cx4a.org>
2015-01-25 08:23:59 +0900

    Version bump and update copyright

--- auto-complete.el
+++ auto-complete.el
@@ -1,11 +1,11 @@
 ;;; auto-complete.el --- Auto Completion for GNU Emacs
 
-;; Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013  Tomohiro Matsuyama
+;; Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015  Tomohiro Matsuyama
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
-;; URL: http://cx4a.org/software/auto-complete
+;; URL: https://github.com/auto-complete/auto-complete
 ;; Keywords: completion, convenience
-;; Version: 1.4.0
+;; Version: 1.5.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@
 
 
 
-(defconst ac-version "1.4.0"
+(defconst ac-version "1.5.0"
   "Version of auto-complete in string format.
 Use `version-to-list' to get version component.")
 
