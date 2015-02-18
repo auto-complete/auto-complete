70770b17168c30fe482467d7219cfbe8650c5e1c
Syohei YOSHIDA <syohex@gmail.com>
2015-02-19 01:00:36 +0900

    Set version same as auto-complete.el

--- auto-complete-config.el
+++ auto-complete-config.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
 ;; Keywords: convenience
-;; Version: 1.4
+;; Version: 1.5.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
